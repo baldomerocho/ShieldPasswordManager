a110bf5b5a961d39e4dde921d05334a4e551fdd4
Baldomero Cho <don.baldo@hotmail.com>
2023-09-12 18:05:10 -0600

    init project

---
+++ README.md
@@ -0,0 +1,7 @@
+# ptf
+
+## FVM
+```
+fvm use 3.10.0
+fvm flutter <command>
+```
---
+++ android/app/src/main/kotlin/cho/baldomero/ptf/MainActivity.kt
@@ -0,0 +1,6 @@
+package cho.baldomero.ptf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
